1eef8bab16a9cf212cdebe76b42be3e031346dbd
jarekTerejko <j.terejko@gmail.com>
2022-10-24 09:32:02 +0200

    create HedingPrimary

---
+++ components/Heading/HeadingPrimary.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const HeadingPrimary = styled.h1`
+  font-size: 56px;
+  padding-top: 66px;
+  line-height: 64px;
+  color: var(--color-dark);
+  margin-bottom: 30px;
+`;
