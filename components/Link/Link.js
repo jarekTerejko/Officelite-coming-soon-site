b5af39a73e6313f2da59b2468a090000ac1d283b
jarekTerejko <j.terejko@gmail.com>
2022-10-24 09:33:23 +0200

    create LinkElement

---
+++ components/Link/Link.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const LinkElement = styled.a`
+  background: var(--color-blue);
+  color: var(--color-white);
+  padding: 18px 41px;
+  font-weight: 500;
+  border-radius: 30px;
+  box-shadow: rgb(100 100 111 / 20%) 0px 7px 29px 0px;
+`;
